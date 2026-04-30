e3c49e726a3b359a90bfac0cb4232271b8c2c9ef
satan-696 <rghvshrma135@gmail.com>
2026-04-30 19:34:21 +0530

    [FIX] render deployment issue

--- server/src/utils/logger/winston.config.js
+++ server/src/utils/logger/winston.config.js
@@ -28,12 +28,17 @@ const format = winston.format.combine(
 
 const transports = [
     new winston.transports.Console(),
-    new winston.transports.File({
-        filename: "logs/error.log",
-        level: "error",
-    }),
-    new winston.transports.File({ filename: "logs/all.log" }),
 ];
+
+if (process.env.NODE_ENV !== "production") {
+    transports.push(
+        new winston.transports.File({
+            filename: "logs/error.log",
+            level: "error",
+        }),
+        new winston.transports.File({ filename: "logs/all.log" })
+    );
+}
 
 const logger = winston.createLogger({
     level: process.env.NODE_ENV === "development" ? "debug" : "warn",
